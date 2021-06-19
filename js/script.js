4f7ad98e652e544a6f60c550e089c3bec07976d3
Abhijit <abhijitbcob96@gmail.com>
2021-06-19 08:27:23 +0530

    cc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+"use strict";
+
+// MOBILE MENU
 const menuToggler = document.querySelector("#menu-toggler");
 const fadeElements = document.querySelectorAll(".has-fade");
 const header = document.querySelector(".header");
@@ -18,5 +21,51 @@ menuToggler.addEventListener("click", function () {
       element.classList.remove("fade-out");
     });
     body.classList.add("noscroll");
+  }
+});
+
+// PRICE TOGGLE ON CLICKING TOGGLE BUTTON
+const toggleButton = document.querySelector("#toggle-element");
+const basicPrice = document.querySelector(".basic-price");
+const proPrice = document.querySelector(".pro-price");
+const businessPrice = document.querySelector(".business-price");
+const subscriptionTypeElems = document.querySelectorAll(".subscription-type");
+
+const monthlyPrices = {
+  subscriptionType: "per month",
+  basic: "$19.00",
+  pro: "$39.00",
+  business: "$99.00",
+};
+
+const yearlyPrices = {
+  subscriptionType: "per year",
+  basic: "$190.00",
+  pro: "$390.00",
+  business: "$990.00",
+};
+
+let subscriptionMonthly = true;
+
+toggleButton.addEventListener("click", function () {
+  if (subscriptionMonthly) {
+    basicPrice.textContent = yearlyPrices.basic;
+    proPrice.textContent = yearlyPrices.pro;
+    businessPrice.textContent = yearlyPrices.business;
+
+    subscriptionTypeElems.forEach((element) => {
+      console.log(element);
+      element.textContent = yearlyPrices.subscriptionType;
+    });
+    subscriptionMonthly = false;
+  } else {
+    basicPrice.textContent = monthlyPrices.basic;
+    proPrice.textContent = monthlyPrices.pro;
+    businessPrice.textContent = monthlyPrices.business;
+
+    subscriptionTypeElems.forEach((element) => {
+      element.textContent = monthlyPrices.subscriptionType;
+    });
+    subscriptionMonthly = true;
   }
 });
